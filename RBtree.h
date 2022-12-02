c769d8625099b5bc7ebd0c6d6c1f23ca36a9bf6c
nek1kek <vip.danya.pavlov.2017@mail.ru>
2022-12-02 11:58:29 +0300

    Добавьте файлы проекта.

---
+++ RBtree.h
@@ -0,0 +1,519 @@
+#pragma once
+#include "exception.h"
+#include "LinkedList.h"
+
+using namespace std;
+
+template<typename T, typename V>//это у нас ключ значение
+struct my_pair {
+	T first;
+	V second;
+	my_pair() = default;
+	~my_pair() = default;
+
+	my_pair(const my_pair<T, V>& other) {
+		this->first = other.first;
+		this->second = other.second;
+	}
+	my_pair(T a, V b) {
+		first = a;
+		second = b;
+	}
+
+	bool operator > (const my_pair<T, V>& other) const {//этот конст говорит, что мы ничего не меняем в классе, если бы был в начале, то возвращал бы константный бул
+		return this->first > other->first;
+	}
+	bool operator < (const my_pair<T, V>& other) const {
+		return this->first < other->first;
+	}
+	bool operator ==  (const my_pair<T, V>& other) const {
+		return this->first == other->first;
+	}
+	bool operator != (const my_pair<T, V>& other) const {
+		return this->first != other->first;
+	}
+};
+
+//https://russianblogs.com/article/1327503951/
+
+template<typename _Key, typename _Value, bool CanChangeValue = true>
+class  RBTree
+{
+private:
+	enum _Color { BLACK, RED, None };
+	struct Node {
+		Node* left;
+		Node* right;
+		Node* parent;
+		std::pair<const _Key, _Value> data;//пара ключ значение
+		_Color color;
+
+		Node() {
+			right = nullptr;
+			left = nullptr;
+			parent = nullptr;
+		}
+
+		Node(const Node& other) : data(other.data) {//двоеточие - а запихнуй сразу в дата инфу с другого
+			this->color = other.color;
+			left = nullptr;
+			right = nullptr;
+			parent = nullptr;
+		}
+		Node(const _Key& key, const _Value& value, const _Color& color, Node* p, Node* l, Node* r) : data(key, value) {
+			this->color = color;
+			left = p;
+			right = l;
+			parent = r;
+		}
+
+	};
+
+	Node* root;
+	size_t _size;
+
+private://функции закрытые от людей, вспомогательные для интерфейсных
+	void leftRotate(Node*& root, Node* x) {// левый поворот
+		Node* y = x->right;
+		x->right = y->left;
+		if (x->right != NULL)
+			x->right->parent = x;
+
+		y->parent = x->parent;
+		if (y->parent == NULL)
+			root = y;
+		else {
+			if (x == x->parent->left)
+				x->parent->left = y;//чтобы поставить у будущего отца 'y' что у это сын его
+			else
+				x->parent->right = y;
+		}
+		y->left = x;
+		x->parent = y;
+	};
+	void rightRotate(Node*& root, Node* y) {// правый поворот
+		Node* x = y->left;
+		y->left = x->right;
+		if (x->right != NULL)
+			x->right->parent = y;
+
+		x->parent = y->parent;
+		if (y->parent == NULL)
+			root = x;
+		else {
+			if (y == y->parent->right)
+				y->parent->right = x;
+			else
+				y->parent->left = x;
+		}
+		x->right = y;
+		y->parent = x;
+	};
+
+	void insert(Node*& root, Node* node) {	// Вставляем узел, внутренний интерфейс
+		Node* x = root;
+		Node* y = NULL;
+		while (x != NULL)
+		{
+			y = x;
+			if (node->data.first > x->data.first)
+				x = x->right;
+			else
+				x = x->left;
+		}
+		node->parent = y;
+		if (y != NULL)
+		{
+			if (node->data.first > y->data.first)
+				y->right = node;
+			else
+				y->left = node;
+		}
+		else
+			root = node;
+		node->color = RED;
+		InsertFixUp(root, node);
+	};
+	void InsertFixUp(Node*& root, Node* node){
+		Node* parent;
+		parent = node->parent;
+		while (node != RBTree::root && parent->color == RED)
+		{
+			Node* gparent = parent->parent;
+			if (gparent->left == parent)
+			{
+				Node* uncle = gparent->right;
+				if (uncle != NULL && uncle->color == RED)
+				{
+					parent->color = BLACK;
+					uncle->color = BLACK;
+					gparent->color = RED;
+					node = gparent;
+					parent = node->parent;
+				}
+				else
+				{
+					if (parent->right == node)
+					{
+						leftRotate(root, parent);
+						swap(node, parent);
+					}
+					rightRotate(root, gparent);
+					gparent->color = RED;
+					parent->color = BLACK;
+					break;
+				}
+			}
+			else
+			{
+				Node* uncle = gparent->left;
+				if (uncle != NULL && uncle->color == RED)
+				{
+					gparent->color = RED;
+					parent->color = BLACK;
+					uncle->color = BLACK;
+
+					node = gparent;
+					parent = node->parent;
+				}
+				else
+				{
+					if (parent->left == node)
+					{
+						rightRotate(root, parent);
+						swap(parent, node);
+					}
+					leftRotate(root, gparent);
+					parent->color = BLACK;
+					gparent->color = RED;
+					break;
+				}
+			}
+		}
+		root->color = BLACK;
+	}
+
+	void destroy(Node*& node) {		//удалить все поддерево с этого узла
+		if (node == NULL)
+			return;
+		destroy(node->left);
+		destroy(node->right);
+		delete node;
+		node = nullptr;
+	}
+	void remove(Node*& root, Node* node){		// Удалить узел как KEY
+		Node* child, * parent;
+		_Color color;
+		// Левый и правый узлы удаленного узла не пусты (не конечные узлы)
+		if (node->left != NULL && node->right != NULL)
+		{
+			Node* replace = node;
+			// Найти узел-преемник (самый нижний левый узел правого поддерева текущего узла)
+			replace = node->right;
+			while (replace->left != NULL)
+			{
+				replace = replace->left;
+			}
+			// Случай, когда удаленный узел не является корневым узлом
+			if (node->parent != NULL)
+			{
+				if (node->parent->left == node)
+					node->parent->left = replace;
+				else
+					node->parent->right = replace;
+			}
+			// Ситуация с корневым узлом
+			else
+				root = replace;
+			// child - это правильный узел, который заменяет узел и является узлом, который требует последующей корректировки
+			// Поскольку замена является преемником, он не может иметь левого дочернего узла
+			// Аналогично, у узла-предшественника не может быть правого дочернего узла
+			child = replace->right;
+			parent = replace->parent;
+			color = replace->color;
+
+			// Удаленный узел является родительским узлом замещающего узла (repalce)
+			if (parent == node)
+				parent = replace;
+			else
+			{
+				// Существование дочернего узла
+				if (child != NULL)
+					child->parent = parent;
+				parent->left = child;
+
+				replace->right = node->right;
+				node->right->parent = replace;
+			}
+			replace->parent = node->parent;
+			replace->color = node->color;
+			replace->left = node->left;
+			node->left->parent = replace;
+			if (color == BLACK)
+				removeFixUp(root, child, parent);
+
+			delete node;
+			return;
+		}
+		// Когда в удаленном узле только левый (правый) узел пуст, найдите дочерний узел удаленного узла
+		if (node->left != NULL)
+			child = node->left;
+		else
+			child = node->right;
+
+		parent = node->parent;
+		color = node->color;
+		if (child)
+		{
+			child->parent = parent;
+		}
+		// Удаленный узел не является корневым узлом
+		if (parent)
+		{
+			if (node == parent->left)
+				parent->left = child;
+			else
+				parent->right = child;
+		}
+		// Удаленный узел является корневым узлом
+		else
+			RBTree::root = child;
+
+		if (color == BLACK)
+		{
+			removeFixUp(root, child, parent);
+		}
+		delete node;
+
+	}
+	void removeFixUp(Node*& root, Node* node, Node* parent){
+		Node* othernode;
+		while ((!node) || node->color == BLACK && node != RBTree::root)
+		{
+			if (parent->left == node)
+			{
+				othernode = parent->right;
+				if (othernode->color == RED)
+				{
+					othernode->color = BLACK;
+					parent->color = RED;
+					leftRotate(root, parent);
+					othernode = parent->right;
+				}
+				else
+				{
+					if (!(othernode->right) || othernode->right->color == BLACK)
+					{
+						othernode->left->color = BLACK;
+						othernode->color = RED;
+						rightRotate(root, othernode);
+						othernode = parent->right;
+					}
+					othernode->color = parent->color;
+					parent->color = BLACK;
+					othernode->right->color = BLACK;
+					leftRotate(root, parent);
+					node = root;
+					break;
+				}
+			}
+			else
+			{
+				othernode = parent->left;
+				if (othernode->color == RED)
+				{
+					othernode->color = BLACK;
+					parent->color = RED;
+					rightRotate(root, parent);
+					othernode = parent->left;
+				}
+				if ((!othernode->left || othernode->left->color == BLACK) && (!othernode->right || othernode->right->color == BLACK))
+				{
+					othernode->color = RED;
+					node = parent;
+					parent = node->parent;
+				}
+				else
+				{
+					if (!(othernode->left) || othernode->left->color == BLACK)
+					{
+						othernode->right->color = BLACK;
+						othernode->color = RED;
+						leftRotate(root, othernode);
+						othernode = parent->left;
+					}
+					othernode->color = parent->color;
+					parent->color = BLACK;
+					othernode->left->color = BLACK;
+					rightRotate(root, parent);
+					node = root;
+					break;
+				}
+			}
+		}
+		if (node)
+			node->color = BLACK;
+	}
+
+	Node* search(Node* node, _Key key) const{
+		if (node == NULL || node->data.first == key)
+			return node;
+		else
+			if (key > node->data.first)
+				return search(node->right, key);
+			else
+				return search(node->left, key);
+	}
+
+	void print(Node* node)const {  //напечатать уже с какого-то узла
+		if (node == NULL)
+			return;
+		if (node->parent == NULL)
+			cout << node->data.first << "(" << node->color << ") is root" << endl;
+		else if (node->parent->left == node)
+		{
+			cout << node->data.first << "(" << node->color << ") is " << node->parent->data.first << "'s " << "left child" << endl;
+		}
+		else
+		{
+			cout << node->data.first << "(" << node->color << ") is " << node->parent->data.first << "'s " << "right child" << endl;
+		}
+		print(node->left);
+		print(node->right);
+	}
+
+	void preOrder(Node* tree)const{
+		if (tree != NULL) {
+			cout << tree->data.first << " ";
+			preOrder(tree->left);
+			preOrder(tree->right);
+		}
+	}
+	void inOrder(Node* tree)const{
+		if (tree != NULL) {
+			inOrder(tree->left);
+			cout << tree->data.first<< " ";
+			inOrder(tree->right);
+		}
+	}
+	void postOrder(Node* tree)const{
+		if (tree != NULL) {
+			postOrder(tree->left);
+			postOrder(tree->right);
+			cout << tree->data.first << " ";
+		}
+	}
+
+
+	bool Equals(const Node* cur, const Node* other) const {
+		if (cur == other && cur == nullptr) return true;
+		if (cur->data != other->data) return false;
+		if (cur->color != other->color) return false;
+		return Equals(cur->left, other->left) && Equals(cur->right, other->right);
+	}
+
+
+	void make_tree(Node* cur, const Node* other) {
+		if (other == nullptr) return;
+		if (other->left != nullptr) {
+			cur->left = new Node(*other->left);
+			cur->left->parent = cur;
+		}
+		if (other->right != nullptr) {
+			cur->right = new Node(*other->right);
+			cur->right->parent = cur;
+		}
+		make_tree(cur->left, other->left);
+		make_tree(cur->right, other->right);
+	
+	}
+
+
+public://функции открытые для людей - интерфейс
+	RBTree() {		//Конструктор
+		root = nullptr;
+		_size = 0;
+	}
+	RBTree(const RBTree<_Key, _Value>& other) {		//Конструктор создавающий объект при получении другого объекта
+		root = new Node(*other.root);
+		_size = other._size;
+		make_tree(root, other.root);
+	}
+	~RBTree() {		// Деструктор
+		destroy(root);
+	}
+
+	//вставка и удаления
+	void insert(const _Key key, const _Value value) {   // Вставляем узел, ключ это значение ключа, внешний интерфейс
+		Node* z = new Node(key, value, RED, NULL, NULL, NULL);
+		insert(root, z);
+	};
+	void remove(_Key key){		// Удалить ключевой узел
+		Node* deletenode = search(root, key);
+		if (deletenode != NULL)
+			remove(root, deletenode);
+	}
+
+	//поиск
+	Node* search(_Key key){
+		return search(root, key);
+	}
+
+	//для печати
+	void print() {		// напечатать дерево
+		if (root == NULL)
+			cout << "empty RBtree";
+		else
+			print(root);
+	}
+
+	//они нужны для обхода
+	void preOrder(){			// Предзаказ обхода печати красного черного дерева
+		if (root == NULL)
+			cout << "empty RBtree";
+		else
+			preOrder(root);
+	};
+	void inOrder(){   // Обход последовательности
+		if (root == NULL)
+			cout << "empty RBtree";
+		else
+			inOrder(root);
+	};
+	void postOrder(){   // пост-заказ обхода	
+		if (root == NULL)
+			cout << "empty RBtree";
+		else
+			postOrder(root);
+	};
+
+	//сравнения деревьев:
+	bool Equals(const RBTree<_Key, _Value>& other) const {
+		return Equals(this->root, other.root);
+	}
+
+	bool operator==(const RBTree<_Key, _Value, CanChangeValue>& other) const {
+		return this->Equals(other);
+	}
+	bool operator!=(const RBTree<_Key, _Value, CanChangeValue>& other) const {
+		return !this->Equals(other);
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Source1.cpp
@@ -0,0 +1,48 @@
+//test.cpp
+#include"Rbtree.h"
+#include "DynamicArray.h"
+#include "LinkedList.h"
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+void a() {
+	vector<int> nums{ 10,40,30,60,90,-1, 70,20,50,80,100 };
+	RBTree<int, int> tree;
+	for (auto num : nums)
+		tree.insert(num, 1);
+	tree.preOrder();
+	cout << endl;
+	tree.inOrder();
+	cout << endl;
+	tree.postOrder();
+	cout << endl;
+	cout << "Найти узел с ключом 30: \n";
+	cout << endl << tree.search(30)->data.first << endl;
+	cout << "Удалить узел с ключом 100 \n";
+	tree.remove(100);
+	tree.preOrder();
+	cout << endl;
+	cout << "\ nКрасные и черные детали дерева: \n";
+	tree.print();
+	cin.get();
+}
+
+void b() {
+	RBTree<int, int> tree1,tree2;
+	vector<int> nums{ 10,40};
+	for (auto num : nums) {
+		tree1.insert(num, 1);
+		tree2.insert(num, 2);
+	}
+	cout << (tree1 == tree2);
+}
+
+
+int main()
+{
+	a();
+}
+
